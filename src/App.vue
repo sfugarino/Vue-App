959093e7324c2088f7a654ee4d2e7917710f27ce
sfugarino <sam@DESKTOP-V5793JI.localdomain>
2020-06-23 00:26:38 -0400

    Initial check-in

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">My Title</span>
+      </md-app-toolbar>
+
+      <md-app-drawer md-permanent="full">
+        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+        <md-list>
+          <md-list-item to="/" exact>
+            <span class="md-list-item-text">Home</span>
+          </md-list-item>
+
+          <md-list-item to="/iot">
+            <span class="md-list-item-text">IoT</span>
+          </md-list-item>
+
+          <md-list-item to="/about">
+            <span class="md-list-item-text">About</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <router-view />
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.md-app {
+  max-height: 400px;
+  border: 1px solid rgba(#000, 0.12);
+}
+
+// Demo purposes only
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+</style>
+
+<script>
+export default {
+  name: "Normal"
+};
+</script>
---
+++ src/views/IoT.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="iot">
+    <h1>This is an iot page</h1>
+  </div>
+</template>
